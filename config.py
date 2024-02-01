2337ad4e553aa9903fab8e929d1092aa2957bca5
ozkan567 <129994112+ozkan567@users.noreply.github.com>
2024-02-01 20:15:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -37,19 +37,19 @@ import random
 
 #
 # 
-api_id = "21814744" #my.telegram.org/apps adresinden alabilirsiniz 
-api_hash = "024ac6bfea2094f8e5e326d778fdcb54" #my.telegram.org/apps adresinden alabilirsiniz
-bot_token = "6351942473:AAF1gagdscrtwRlqxbOB8tfUb6Da-hS6ISw" #botfatherdan alabilirsiniz
+api_id = "14209813" #my.telegram.org/apps adresinden alabilirsiniz 
+api_hash = "f6fc4af6fa07543ace7bcf771e89cdd2" #my.telegram.org/apps adresinden alabilirsiniz
+bot_token = "6716666574:AAEgio4eFtQW-2KzpfI6OCiqb4m9UTsMLng" #botfatherdan alabilirsiniz
 
 client = TelegramClient("Samil", api_id, api_hash).start(bot_token=bot_token)
 
-USERNAME = "etiketcavres_bot" #botunuzun kullanıcı adı
-log_qrup = -1001915718534 #log qrupunuzun idsi
-startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için çavreş tarafından özel geliştirilen bir botum" #start mesajınız
+USERNAME = "etiketsudenaz_bot" #botunuzun kullanıcı adı
+log_qrup = -1001831099952 #log qrupunuzun idsi
+startmesaj = "💌 arkadaşlarınızı nasıl etiketlemek istersiniz ...\n📚 ben gruplarınız için parliament tarafından özel geliştirilen bir botum" #start mesajınız
 komutlar = "🇦🇿 Bütün etiketler ;\n\n» /utag   <  mesaj  >\n   - üyeleri 5'li etiketler .\n\n» /tag   <  mesaj  >\n   - üyeleri tek tek etiketler .\n\n» /atag   <  mesaj  >\n   - Adminleri etiketler .\n\n» /etag   <  mesaj  >\n   - üyeleri emoji ile etiketler .\n\n» /stag   <  mesaj  >\n   - üyeleri güzel söz ile etiketler .\n\n» /cancel = >\n   - etiketleme işlemi durdurulur ." #komutların olduğu mesaj
 qrupstart = "• şuan aktif çalışmaktayım  . . .\n\n• üyeleri etiketlemek için grubunuza ekleyin . . ." #aktif olduğunda gruba gelen mesaj
-support = "cavresozel" #destek qrupunuzun kullanıcı adı
-sahib = "rahatsizetmeyiniz34" #sahibinizin kullanıcı adı
+support = "Medikalsohbettr" #destek qrupunuzun kullanıcı adı
+sahib = "parliamenttr" #sahibinizin kullanıcı adı
 noadmin = "➻ Üzgünüm Ama Yönetici Değilsiniz ." #yönetici olmayanlar için mesaj
 
 #
